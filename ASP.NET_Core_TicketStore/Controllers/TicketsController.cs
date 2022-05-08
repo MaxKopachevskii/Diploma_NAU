ce46603a926f10bb2a39bdc5aedeaaffa4be0fba
Maksym Kopachevskyi <max169665@gmail.com>
2022-05-08 18:28:45 +0300

    Added categories to application

--- ASP.NET_Core_TicketStore/Controllers/TicketsController.cs
+++ ASP.NET_Core_TicketStore/Controllers/TicketsController.cs
@@ -15,11 +15,10 @@ namespace ASP.NET_Core_TicketStore.Controllers
 
         enum TicketCategories
         {
-            Programming = 1,
-            Psyhology = 2,
-            Literature = 3,
-            Medical = 4,
-            Fantastic = 5
+            WarmRoutes = 1,
+            ColdRoutes = 2,
+            NatureRoutes = 3,
+            CityRoutes = 4
         }
 
         public TicketsController(ITicketsUnitOfWork context)
@@ -84,91 +83,74 @@ namespace ASP.NET_Core_TicketStore.Controllers
             return View(viewModel);
         }
 
-        /*public async Task<IActionResult> AllProgrammingBooks(int page = 1)
+        public async Task<IActionResult> AllWarmRoutes(int page = 1)
         {
-            int pageSize = 6;   // количество элементов на странице
+            int pageSize = 12;   // количество элементов на странице
 
-            IQueryable<Book> source = unitOfWork.Books.GetAllBooksByCategory((int)Categories.Programming);
+            IQueryable<Ticket> source = unitOfWork.Tickets.GetAllTicketsByCategory((int)TicketCategories.WarmRoutes);
             var count = await source.CountAsync();
             var items = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
 
             PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
-            IndexViewModel viewModel = new IndexViewModel
+            TicketIndexViewModel viewModel = new TicketIndexViewModel
             {
                 PageViewModel = pageViewModel,
-                Books = items
+                Tickets = items
             };
             return View(viewModel);
         }
-
-        public async Task<IActionResult> AllPsyhologyBooks(int page = 1)
+        
+        public async Task<IActionResult> AllColdRoutes(int page = 1)
         {
-            int pageSize = 6;   // количество элементов на странице
+            int pageSize = 12;   // количество элементов на странице
 
-            IQueryable<Book> source = unitOfWork.Books.GetAllBooksByCategory((int)Categories.Psyhology);
+            IQueryable<Ticket> source = unitOfWork.Tickets.GetAllTicketsByCategory((int)TicketCategories.ColdRoutes);
             var count = await source.CountAsync();
             var items = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
 
             PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
-            IndexViewModel viewModel = new IndexViewModel
+            TicketIndexViewModel viewModel = new TicketIndexViewModel
             {
                 PageViewModel = pageViewModel,
-                Books = items
+                Tickets = items
             };
             return View(viewModel);
         }
-
-        public async Task<IActionResult> AllLiteratureBooks(int page = 1)
+        
+        public async Task<IActionResult> AllNatureRoutes(int page = 1)
         {
-            int pageSize = 6;   // количество элементов на странице
+            int pageSize = 12;   // количество элементов на странице
 
-            IQueryable<Book> source = unitOfWork.Books.GetAllBooksByCategory((int)Categories.Literature);
+            IQueryable<Ticket> source = unitOfWork.Tickets.GetAllTicketsByCategory((int)TicketCategories.NatureRoutes);
             var count = await source.CountAsync();
             var items = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
 
             PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
-            IndexViewModel viewModel = new IndexViewModel
+            TicketIndexViewModel viewModel = new TicketIndexViewModel
             {
                 PageViewModel = pageViewModel,
-                Books = items
+                Tickets = items
             };
             return View(viewModel);
         }
-
-
-        public async Task<IActionResult> AllMedicalBooks(int page = 1)
+        
+        public async Task<IActionResult> AllCityRoutes(int page = 1)
         {
-            int pageSize = 6;   // количество элементов на странице
+            int pageSize = 12;   // количество элементов на странице
 
-            IQueryable<Book> source = unitOfWork.Books.GetAllBooksByCategory((int)Categories.Medical);
+            IQueryable<Ticket> source = unitOfWork.Tickets.GetAllTicketsByCategory((int)TicketCategories.CityRoutes);
             var count = await source.CountAsync();
             var items = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
 
             PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
-            IndexViewModel viewModel = new IndexViewModel
+            TicketIndexViewModel viewModel = new TicketIndexViewModel
             {
                 PageViewModel = pageViewModel,
-                Books = items
+                Tickets = items
             };
             return View(viewModel);
         }
-
-        public async Task<IActionResult> AllFantasticBooks(int page = 1)
-        {
-            int pageSize = 6;   // количество элементов на странице
-
-            IQueryable<Book> source = unitOfWork.Books.GetAllBooksByCategory((int)Categories.Fantastic);
-            var count = await source.CountAsync();
-            var items = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
-
-            PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
-            IndexViewModel viewModel = new IndexViewModel
-            {
-                PageViewModel = pageViewModel,
-                Books = items
-            };
-            return View(viewModel);
-        }*/
+        
 
         public IActionResult Basket()
         {
